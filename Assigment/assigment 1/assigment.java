090f47048d373cf8e3cab8ad9f577634d9fa144e
kaifonavt <artur.mikha@gmail.com>
2025-12-12 11:44:07 +0500

    starting assig 1

---
+++ Assigment/assigment 1/assigment.java
@@ -0,0 +1,10 @@
+public class Rectangle
+{
+     private double width;
+     private double height;
+     private int id;
+     private static int idGen;
+     public Rectangle(double width, double height){
+
+     }
+}
