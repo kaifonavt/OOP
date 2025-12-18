ebde6b9a75dca1b3e822e0eaab9301db86472228
spark1 <artur.mikha@gmail.com>
2025-12-18 22:24:27 +0500

    book started

---
+++ Assigment/assigment 1/book.java
@@ -0,0 +1,10 @@
+import java.time.Year;
+
+public class Book {
+    private int id;
+    private static int idGen = 1;
+    private String title;
+    private String author;
+    private int year;
+    private boolean available;
+}
